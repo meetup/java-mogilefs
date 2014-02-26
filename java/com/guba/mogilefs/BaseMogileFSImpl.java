d8b17e443b2a34c5666adbe4eb01295cc03a9bff
softprops <d.tangren@gmail.com>
2014-02-26 11:51:30 -0500

    remove older closings

--- java/com/guba/mogilefs/BaseMogileFSImpl.java
+++ java/com/guba/mogilefs/BaseMogileFSImpl.java
@@ -215,9 +215,6 @@ public abstract class BaseMogileFSImpl implements MogileFS {
 	                    while ((count = in.read(buffer)) >= 0) {
 	                        out.write(buffer, 0, count);
 	                    }
-	        
-	                    out.close();
-	                    in.close();
 	                    
 	                    // success!
 	                    return;
